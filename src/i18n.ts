29092b44c9042a0701573d5caaeb73544bf9a814
roy.zhu <oldroy@163.com>
2024-05-11 13:18:56 +0800

    修改引用方式，防止electron引用路径错误

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,11 +4,26 @@ import { initReactI18next } from "react-i18next"
 import Backend from "i18next-http-backend"
 import LanguageDetector from "i18next-browser-languagedetector"
 
+// import translationEN from '@/locales/en/translation.json';
+import translationCN from '@/locales/cn/translation.json'
+
+// the translations
+const resources = {
+  // en: {
+  //   translation: translationEN
+  // },
+  cn: {
+    translation: translationCN
+  }
+};
+
+// https://react.i18next.com/legacy-v9/step-by-step-guide
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources,
     fallbackLng: "en",
     interpolation: {
       escapeValue: false,
